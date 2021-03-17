71246481186a1b0c9d2da2e7e9ebe6afa8bd0f54
serber <albert.zakiev@gmail.com>
2021-03-17 12:29:33 +0300

    Проверка работоспособности веб сервиса + файл конфигурации

--- WebMarket/Startup.cs
+++ WebMarket/Startup.cs
@@ -4,15 +4,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebMarket
 {
     public class Startup
     {
+        private IConfiguration _configuration { get; }
+
+        public Startup(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
@@ -22,18 +25,22 @@ namespace WebMarket
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //блок отбрабатывающий исключени€, если работа в режиме разработки
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            //маршрутизаци€ приложени€ (к какому ресурсу приложени€ обратилс€ браузер)
             app.UseRouting();
 
+            //конфигураци€ адресов, на которые откправл€ютс€ запросы и возвращаютс€ ответы
             app.UseEndpoints(endpoints =>
             {
+                //запрос - ответ ("/" - _configuration["Greetings"])
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(_configuration["Greetings"]);
                 });
             });
         }

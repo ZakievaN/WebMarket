41adf0e806054da33a34fb5f5487314872bdca82
serber <albert.zakiev@gmail.com>
2021-03-17 14:01:16 +0300

    Первый контроллер

---
+++ WebMarket/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMarket.Controllers
+{ 
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("First controller action");
+        }
+
+        public IActionResult SecondAction(string id)
+        {
+            return Content($"Action with value id: {id}");
+        }
+    }
+}

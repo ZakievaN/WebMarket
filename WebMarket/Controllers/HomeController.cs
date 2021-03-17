b41c4fbbd88445b0f59877b5a97b712240629876
serber <albert.zakiev@gmail.com>
2021-03-17 14:28:08 +0300

    Визуализация таблицы сотрудников

--- WebMarket/Controllers/HomeController.cs
+++ WebMarket/Controllers/HomeController.cs
@@ -1,13 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using WebMarket.Models;
 
 namespace WebMarket.Controllers
-{ 
+{
     public class HomeController : Controller
     {
+        private static readonly List<Employee> _employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", FirstName = "Иван", Patronymic = "Иванович", Age = 19 },
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Петр", Patronymic = "Петрович", Age = 25 },
+            new Employee { Id = 3, LastName = "Сидоров", FirstName = "Сидор", Patronymic = "Сидорович", Age = 46 }
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +21,11 @@ namespace WebMarket.Controllers
         public IActionResult SecondAction(string id)
         {
             return Content($"Action with value id: {id}");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(_employees);
         }
     }
 }

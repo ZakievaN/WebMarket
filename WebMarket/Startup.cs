815565e1ac67358f9377c4c6c46c027ef1ccccb6
serber <albert.zakiev@gmail.com>
2021-03-17 21:34:49 +0300

    рефакторинг

--- WebMarket/Startup.cs
+++ WebMarket/Startup.cs
@@ -16,31 +16,30 @@ namespace WebMarket
             _configuration = Configuration;
         }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //блок отбрабатывающий исключения, если работа в режиме разработки
+            //  Блок обрабатывающий исключения, если работа в режиме разработки
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            //заставляет сервер отдавать статическое содержимое
+            //  Заставляет сервер отдавать статическое содержимое
             app.UseStaticFiles();
 
-            //маршрутизация приложения (к какому ресурсу приложения обратился браузер)
+            //  Маршрутизация приложения (к какому ресурсу приложения обратился браузер)
             app.UseRouting();
 
-            //конфигурация адресов, на которые откправляются запросы и возвращаются ответы
+            //  Конфигурация адресов, на которые откправляются запросы и возвращаются ответы
             app.UseEndpoints(endpoints =>
             {
-                //запрос - ответ ("/" - _configuration["Greetings"])
+                //  Запрос - ответ ("/" - _configuration["Greetings"])
                 endpoints.MapGet("/Greetings", async context =>
                 {
                     await context.Response.WriteAsync(_configuration["Greetings"]);

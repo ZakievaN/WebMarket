5feb5a2ed6efbbf916c6606dfec1fc6e310cecde
zakievan <nafiska.k@gmail.com>
2021-04-07 23:04:45 +0300

    Контекст базы данных

---
+++ WebMarket.DAL/Context/WebMarketDB.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebMarketDomain.Entities;
+
+namespace WebMarket.DAL.Context
+{
+    public class WebMarketDB : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Section> Sections { get; set; }
+
+        public WebMarketDB(DbContextOptions<WebMarketDB> options) : base(options)
+        {
+
+        }
+    }
+}

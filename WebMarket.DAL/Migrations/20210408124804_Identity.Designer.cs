55030e4d33086582ca00516ac4aa3e733fb48b18
zakievan <nafiska.k@gmail.com>
2021-04-08 15:50:21 +0300

    Миграция БД

--- WebMarket.DAL/Migrations/20210408124804_Identity.Designer.cs
+++ WebMarket.DAL/Migrations/20210408124804_Identity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebMarket.DAL.Context;
 
 namespace WebMarket.DAL.Migrations
 {
     [DbContext(typeof(WebMarketDB))]
-    partial class WebMarketDBModelSnapshot : ModelSnapshot
+    [Migration("20210408124804_Identity")]
+    partial class Identity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

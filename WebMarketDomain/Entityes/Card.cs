ed485496073c2398ac51f15c1fb7453a4c90e534
zakievan <nafiska.k@gmail.com>
2021-04-09 17:45:35 +0300

    Сущность корзины

---
+++ WebMarketDomain/Entityes/Card.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace WebMarketDomain.Entityes
+{
+    public class Card
+    {
+        public ICollection<CardItem> Items { get; set; } = new List<CardItem>();
+
+        public int ItemsCount()
+        {
+            return Items?.Sum(Items => Items.Quality) ?? 0;
+        }
+    }
+}
---
+++ WebMarketDomain/Entityes/CardItem.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebMarketDomain.Entityes
+{
+    public class CardItem
+    {
+        public int ProductId { get; set; }
+
+        public int Quality { get; set; } = 1;
+    }
+}

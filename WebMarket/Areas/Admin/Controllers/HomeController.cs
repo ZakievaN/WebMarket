643452865bdab5d74155ec37ff4b0049b57c0d2d
zakievan <nafiska.k@gmail.com>
2021-04-12 23:14:09 +0300

    Доступ к области администрации

--- WebMarket/Areas/Admin/Controllers/HomeController.cs
+++ WebMarket/Areas/Admin/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using WebMarketDomain.Entityes.Identity;
 
 namespace WebMarket.Areas.Admin.Controllers
 {
-    [Area("Admin")]
+    [Area("Admin"), Authorize(Roles = Role.Administrator)]
     public class HomeController : Controller
     {
         public IActionResult Index()

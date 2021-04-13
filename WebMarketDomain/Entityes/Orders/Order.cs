77cb654ab5ca9cdb0ada6093e5138d700df46c54
zakievan <nafiska.k@gmail.com>
2021-04-13 23:36:20 +0300

    Определение сущности заказов

---
+++ WebMarketDomain/Entityes/Orders/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using WebMarketDomain.Entityes.Base;
+using WebMarketDomain.Entityes.Identity;
+
+namespace WebMarketDomain.Entityes.Orders
+{
+    public class Order : NamedEntity
+    {
+        public User User { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Address { get; set; }
+
+        public DateTime Date { get; set; } = DateTime.Now;
+
+        public ICollection<OrderItem> Items { get; set; } = new HashSet<OrderItem>();
+    }
+
+
+}
---
+++ WebMarketDomain/Entityes/Orders/OrderItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using WebMarketDomain.Entityes.Base;
+
+namespace WebMarketDomain.Entityes.Orders
+{
+    public class OrderItem : Entity
+    {
+        [Required]
+        public Order Order { get; set; }
+
+        [Required]
+        public Product Product { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        public int Quantity { get; set; }
+
+        [NotMapped]
+        public decimal TotalItemPrice => Price * Quantity;
+    }
+}
